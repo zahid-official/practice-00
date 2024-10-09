3e588d4b8a3a7b4ab6ecfce75353955ced31624a
zahid-official <zahid.official8@gmail.com>
2024-10-09 23:36:22 +0600

    Module-21

---
+++ programming-hero/module-21/arguments.js
@@ -0,0 +1,53 @@
+
+
+// string argument
+// function isEvenString(checkStrling){
+//     let size = checkStrling.length;
+//     if(size % 2 ===0){
+//         return 'word: ' + checkStrling +', size: '+ size + ', even: ' + true;
+//     }
+//     return 'word: ' + checkStrling + ', size: '+ size + ', even: ' + false;
+// }
+// console.log(isEvenString('Africa'));
+// console.log(isEvenString('Bangladeshi'));
+
+
+
+
+// boolean argument
+// function doubOrTri(number, double){
+//     if( double === true){
+//         const result = number * 2;
+//         return result;
+//     }
+//     else{
+//         const result = number * 3;
+//         return result;
+//     }
+// }
+// console.log(doubOrTri(5, true));
+// console.log(doubOrTri(5, false));
+
+
+
+
+
+// array argument
+// function elements(number){
+//     return number.length
+// }
+// console.log(elements( [1, 25, 12, 6, 32, 65] ));
+
+
+
+
+
+
+
+
+//object argument
+// function keyOfValue(getAge){
+//     const age = getAge.age;
+//     return age;
+// }
+// console.log(keyOfValue( {name: 'zahid', age: 25, height: 5.7, weight: 75} ));
