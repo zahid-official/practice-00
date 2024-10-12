e149d6adf51a0cb0034751475fdd0734ca3699db
zahid-official <zahid.official8@gmail.com>
2024-10-12 19:55:31 +0600

    Function

---
+++ programming-hero/module-21/function.js
@@ -0,0 +1,71 @@
+// function
+// function square (number){
+//     const getSquare = number * number;
+//     console.log('square is:', getSquare);
+// }
+// square(6);
+
+// multiple parameter
+// function add(a, b, c, d){
+//     let sum = a + b + c + d
+//     console.log('total:', sum);
+// }
+// add(2, 14, 9, 16);
+
+
+
+//argument
+// let salary = 25;
+// let expends = 18;
+// function remain(num1, num2){
+//     const total = num1 - num2;
+//     console.log(total); 
+// }
+// remain(salary, expends);
+
+
+
+/* 
+// return 
+function multipie(money){
+    const result = money * 15;
+    return result;  //return না করলে কি হয় তা দেখতে রিটার্নকে comment করুন
+}
+console.log(multipie(5));
+
+// shortcut of return 
+function multipie(money){
+    return money * 15; 
+}
+console.log(multipie(5));
+ */
+
+
+
+
+
+/* 
+// conditional return
+function isOdd(digit){
+    if(digit % 2 === 1){
+        return 'this number is odd: ' + true;
+    }
+    else{
+        return 'this number is odd: ' +  false;
+    }
+}
+console.log(isOdd(11));
+
+
+// shortcut of conditional return
+function isOdd(digit){
+    if(digit % 2 === 1){
+        return 'this number is odd: ' + true;
+    }
+    return 'this number is odd: ' +  false;
+}
+console.log(isOdd(15));
+ */
+
+
+
