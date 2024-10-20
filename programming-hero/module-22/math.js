933a470b5e54a152229ff0998d176252de86715e
zahid-official <zahid.official8@gmail.com>
2024-10-20 21:16:16 +0600

    Math

---
+++ programming-hero/module-22/math.js
@@ -0,0 +1,40 @@
+
+// get highest and lowest number
+// max number
+// const maxNumber = Math.max(1, 2, 3, 4, 50, 12, 25)
+// console.log(maxNumber);
+
+// min number
+// const minNumber = Math.min(1, 2, 3, 4, 50, 12, 25)
+// console.log(minNumber);
+
+
+//absolute number
+// const difference = Math.abs(5 - 8);
+// console.log(difference);
+
+
+//round number
+// const round = Math.round(4.5);
+// console.log(round);
+
+
+//floor number
+// const floor = Math.floor(4.99);
+// console.log(floor);
+
+
+//celing number
+// const celing = Math.ceil(4.001);
+// console.log(celing);
+
+
+
+//random float number
+// const random = Math.random();
+// console.log(random);
+
+
+// get a random intizer Number
+const randomNumber = Math.round(Math.random() * 10);
+console.log(randomNumber);
