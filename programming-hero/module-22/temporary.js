5a40e6253eb95fa0a8e9a64fd8dcd6e50638fb4c
zahid-official <zahid.official8@gmail.com>
2024-10-25 16:07:26 +0600

    Temporary

---
+++ programming-hero/module-22/temporary.js
@@ -0,0 +1,21 @@
+/* let a = 5;
+let b = 10;
+
+const temp = a;
+
+console.log(a,b);
+
+//swap value of a to b and value of b to a using by temp variable
+a = b;
+b = temp;
+console.log(a,b);
+ */
+
+// destructing
+let x = 15;
+let y = 24;
+console.log(x,y);
+
+//swap value of x to y and value of y to x using by destructing system
+[x,y] = [y,x]
+console.log(x,y);
