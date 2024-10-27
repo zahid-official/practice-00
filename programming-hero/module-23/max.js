f8d958d9fb90edbcdbab42b12fda0dd6d0675710
zahid-official <zahid.official8@gmail.com>
2024-10-27 22:18:05 +0600

    Max

---
+++ programming-hero/module-23/max.js
@@ -0,0 +1,54 @@
+
+// max between 2 numbers
+/* function getMax(num1, num2){
+    if (num1 > num2) {
+        return num1;
+    }
+    else{
+        return num2;
+    }
+}
+
+const max1 = (getMax(76, 22));
+const max2 = (getMax(14, 45));
+const ultraMax = getMax(max1, max2)
+console.log(ultraMax); */
+
+
+
+
+
+// max between 3 numbers
+/* function getMaxOne(num1, num2, num3){
+    if (num1 > num2 && num1 > num3) {
+        return num1;
+    }
+    else if(num2 > num1 && num2 > num3){
+        return num2
+    }
+    else{
+        return num3
+    }
+}
+
+console.log(getMaxOne(45, 63, 79)); */
+
+
+
+
+
+
+
+
+// max by Math.max()
+const rahim = 75;
+const fahim = 58;
+const mahim = 36;
+const tuhin = 14;
+const ruhin = 91;
+const zahid = 98;
+const nahid = 63;
+const walid = 78;
+
+const max = Math.max(rahim, fahim, mahim, tuhin, ruhin, zahid, nahid, walid);
+console.log(max);
