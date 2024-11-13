a996c8c7f612fa0d846da535a13d1ba6c342a7ee
zahid-official <zahid.official8@gmail.com>
2024-11-13 18:05:39 +0600

    Adding Root

--- programming-hero/module-50/app-react/src/main.jsx
+++ programming-hero/module-50/app-react/src/main.jsx
@@ -1,13 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./Root.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <Root />,
+    children: [
+      {
+
+      },
+    ]
   },
 ]);
 
