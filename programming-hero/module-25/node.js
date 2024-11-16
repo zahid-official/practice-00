daab280627b993409f4333932e7ba274e78c7887
zahid-official <zahid.official8@gmail.com>
2024-11-16 22:01:50 +0600

    NodeJS

---
+++ programming-hero/module-25/node.js
@@ -0,0 +1,16 @@
+/* let unity = document.getElementById('unity');
+console.log(unity.parentNode);
+console.log(unity.childNodes);
+console.log(unity.firstChild);
+console.log(unity.lastChild);
+console.log(unity.childNodes[1]);
+console.log(unity.childNodes[2].nextSibling);
+console.log(unity.childNodes[2].previousSibling);
+console.log(unity.childNodes[5].childNodes); */
+
+
+// let container = document.querySelector('#text-container');
+// let created = document.createElement('li');
+// created.innerText = 'adding text to new element by js';
+// container.appendChild(created);
+
