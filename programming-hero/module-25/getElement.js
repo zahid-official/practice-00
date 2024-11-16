493eeb6eb7eaa0932e45a1f96b8f8305e4fac072
zahid-official <zahid.official8@gmail.com>
2024-11-16 22:01:24 +0600

    getElement

---
+++ programming-hero/module-25/getElement.js
@@ -0,0 +1,44 @@
+
+
+/*
+//style by js
+let styleDynamic = document.getElementById('ipsum-content').style.color = '#fb8500';
+console.log(styleDynamic);
+
+let classSelector = document.getElementsByClassName('selector');
+for(let element of classSelector){
+    element.style.backgroundColor = '#8d99ae';
+    element.style.fontSize = '18px';
+    console.log(element);
+}
+ */
+
+
+
+
+
+/* 
+//get & set attribute
+// let attributeCatch = document.getElementById('attribute');
+// attributeCatch.getAttribute('style');
+// attributeCatch.setAttribute('title', 'attribute set by JS');
+// console.log(attributeCatch);
+
+
+//add & remove class
+// let allClass = document.getElementById('arc');
+// allClass.classList;
+// allClass.classList.remove('media');
+// allClass.classList.add('digital-world');
+// console.log(allClass);
+ */
+
+
+
+
+
+
+/* //get innerHtml
+// let catchHtml = document.getElementsByClassName('selector')[0].innerHTML;
+// catchHtml = '<h2> Change html by js'
+// console.log(catchHtml); */
