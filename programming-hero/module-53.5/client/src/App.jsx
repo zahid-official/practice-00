93214e604ed3f32dc0293d3b4d9cf840d03b1a9c
zahid-official <zahid.official8@gmail.com>
2024-11-26 15:44:27 +0600

    Routes Setup

--- programming-hero/module-53.5/client/src/App.jsx
+++ programming-hero/module-53.5/client/src/App.jsx
@@ -1,8 +1,8 @@
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <div className="p-8">
+      <h1 className="text-3xl font-bold ">Hello world!</h1>
+    </div>
   );
 }
 
