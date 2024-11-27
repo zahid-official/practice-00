43a45c0f0ca7df1a9c4e4330abbd9882045e8ca6
zahid-official <zahid.official8@gmail.com>
2024-11-28 05:06:57 +0600

    Recive Data from Client

--- programming-hero/module-54/client/src/App.jsx
+++ programming-hero/module-54/client/src/App.jsx
@@ -4,6 +4,28 @@ import "./App.css";
 function App() {
   const [user, setUser] = useState([]);
 
+  //handleSubmit
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const newUser = {name, email};
+
+    fetch('http://localhost:3000/users', {
+      method: 'post',
+      headers: {
+        'Content-Type' : 'application/json'
+      },
+      body: JSON.stringify(newUser)
+    })
+
+    .then(res => res.json())
+    .then(data => setUser([...user], data))
+    form.reset();
+  }
+
   useEffect(()=>{
     fetch('http://localhost:3000/users')
     .then(res => res.json())
@@ -14,6 +36,17 @@ function App() {
     <>
       <h1 className="text-3xl font-bold underline">User Management System</h1>
       <p>Total Users: {user.length}</p>
+
+      <form onSubmit={handleSubmit}>
+        <h2>Add User Form</h2>
+        <input type="text" name="name" placeholder="name" /><br /><br />
+        <input type="email" name="email" placeholder="email" /><br /><br />
+        <input type="submit" value="Add User" />
+      </form>
+
+      <div>
+        {user.map(item => <p key={item.id}> {item.id}. {item.name} || {item.email} </p>)}
+      </div>
     </>
   );
 }
--- programming-hero/module-54/server/index.js
+++ programming-hero/module-54/server/index.js
@@ -1,24 +1,31 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
 
 const users = [
-  {id: 1, name: 'Fahim', email: 'fahim@gmail.com'},
-  {id: 2, name: 'Rahim', email: 'rahim@gmail.com'},
-  {id: 3, name: 'Mahin', email: 'mahin@gmail.com'},
-]
+  { id: 1, name: "Fahim", email: "fahim@gmail.com" },
+  { id: 2, name: "Rahim", email: "rahim@gmail.com" },
+  { id: 3, name: "Mahin", email: "mahin@gmail.com" },
+];
 
 app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Data from Server')
-})
+app.get("/", (req, res) => {
+  res.send("Data from Server");
+});
 
-app.get('/users', (req, res)=>{
-  res.send(users)
-})
+app.get("/users", (req, res) => {
+  res.send(users);
+});
+
+app.post("/users", (req, res) => {
+  const newUser = req.body;
+  newUser.id = users.length + 1;
+  users.push(newUser);
+});
 
 app.listen(port, () => {
-  console.log(`Port running on: ${port}`)
-})
+  console.log(`Port running on: ${port}`);
+});
