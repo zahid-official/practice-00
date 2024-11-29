221ef94a1d0cda482a9be1e8703577fee4878f27
zahid-official <zahid.official8@gmail.com>
2024-11-29 18:24:52 +0600

    Client

--- programming-hero/module-55/server/index.js
+++ programming-hero/module-55/server/index.js
@@ -28,15 +28,29 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const database  = client.db('usersDB');
+    const usersList = database.collection('users');
+
+    // receive data from client to server and send that to DB
+    app.post('/users', async(req, res) => {
+      const user = req.body;
+      console.log(user)
+      const result = await usersList.insertOne(user);
+      res.send(result);
+    })
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
-run().catch(console.dir);
+run().catch(console.log);
 
 
 
