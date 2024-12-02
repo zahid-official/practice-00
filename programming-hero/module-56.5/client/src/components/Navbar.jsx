6ae002780f351555d486a7ee92828f54e32ba474
zahid-official <zahid.official8@gmail.com>
2024-12-03 01:08:37 +0600

    Navbar

--- programming-hero/module-56.5/client/src/components/Navbar.jsx
+++ programming-hero/module-56.5/client/src/components/Navbar.jsx
@@ -1,33 +1,84 @@
 import logo from "/assets/01.logo.png";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
+  const links = (
+    <>
+      <li className="text-lg font-semibold">
+        <NavLink to={"/"}>Home</NavLink>
+      </li>
+      <li className="text-lg font-semibold">
+        <NavLink to={"/add"}>Coffee</NavLink>
+      </li>
+      <li className="text-lg font-semibold">
+        <NavLink to={"/update"}>Update</NavLink>
+      </li>
+      <li className="text-lg font-semibold">
+        <NavLink to={"/user"}>User</NavLink>
+      </li>
+    </>
+  );
   return (
-    <div
-      className={`flex items-center justify-between py-6 md:px-20 px-5 bg-[url(/assets/02.bg.jpg)] bg-center`}
-    >
-      <div className={"flex flex-1 gap-1.5 items-center sm:justify-start justify-center"}>
-        <img src={logo} alt="logo" className="h-20" />
-        <h1 className="sm:text-5xl sm:block hidden text-4xl font-semibold rancho text-white">
-          {" "}
-          Espresso Emporium
-        </h1>
-      </div>
+    <>
+      <div className="navbar justify-between py-6 xl:px-20 md:px-10 sm:px-4 bg-[url(/assets/02.bg.jpg)] bg-center text-white">
+        <div className="">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu bg-[#7e644a] menu-sm dropdown-content rounded-box z-[1] mt-3 w-52 p-2 shadow text-black"
+            >
+              {links}
+            </ul>
+          </div>
+          <Link to={"/"}>
+            <div
+              className={
+                "flex flex-1 gap-1.5 items-center sm:justify-start justify-center"
+              }
+            >
+              <img src={logo} alt="logo" className="h-20" />
+              <h1 className="md:text-5xl sm:block hidden text-4xl font-semibold rancho text-white">
+                Espresso Emporium
+              </h1>
+            </div>
+          </Link>
+        </div>
 
-      <div className="space-x-3">
-        <Link to={"/login"}>
-          <button className="btn rounded rancho text-xl bg-[#c99d62] border-none hover:text-white hover:bg-transparent hover:outline hover:outline-1 hover:outline-white transition px-8 duration-500">
-            Login
-          </button>
-        </Link>
+        <div className="hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">{links}</ul>
+        </div>
 
-        <Link to={"/login"}>
-          <button className="btn rounded rancho text-xl bg-[#c99d62] border-none hover:text-white hover:bg-transparent hover:outline hover:outline-1 hover:outline-white transition px-6 duration-500">
-            Sign Up
-          </button>
-        </Link>
+        <div className="space-x-3">
+          <Link to={"/login"}>
+            <button className="btn rounded rancho text-xl bg-[#c99d62] border-none hover:text-white hover:bg-transparent hover:outline hover:outline-1 hover:outline-white transition sm:px-8 px-6 duration-500">
+              Login
+            </button>
+          </Link>
+
+          <Link to={"/login"}>
+            <button className="btn rounded rancho text-xl bg-[#c99d62] border-none hover:text-white hover:bg-transparent hover:outline hover:outline-1 hover:outline-white transition sm:px-6 duration-500">
+              Sign Up
+            </button>
+          </Link>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
