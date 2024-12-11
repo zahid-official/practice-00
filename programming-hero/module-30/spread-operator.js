44aeb3bfc14bd27adb6983ccdb71000322d73248
zahid-official <zahid.official8@gmail.com>
2024-12-11 10:28:28 +0600

    Spread Operator

---
+++ programming-hero/module-30/spread-operator.js
@@ -0,0 +1,12 @@
+const numbers = [12, 32, 15, 28, 43];
+const maximum = Math.max(...numbers);
+// console.log(maximum);
+
+
+const age = [8, 12, 16, 18, 21];
+const storeAge = [...age, 45];
+const newOne = age;
+newOne.push(25, 30);
+// console.log(storeAge);
+
+
