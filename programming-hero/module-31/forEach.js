9be39ca69c7a0c4c0b2e48070cce22905ff5ad81
zahid-official <zahid.official8@gmail.com>
2024-12-12 19:03:24 +0600

    ForEach

---
+++ programming-hero/module-31/forEach.js
@@ -0,0 +1,21 @@
+const numbers = [1, 2, 3, 4, 5];
+
+/* //Map
+const doubled = numbers.map(function(num){
+        return num * 2
+    });
+console.log(doubled);
+
+// forEach undefined
+const multiply = numbers.forEach(function(num){
+    return num * 2
+});
+console.log(multiply); */
+
+
+// forEach parameter output
+const multiplication = numbers.map(function(num){
+    return num * 2;
+});
+console.log(multiplication);
+console.log(numbers);
