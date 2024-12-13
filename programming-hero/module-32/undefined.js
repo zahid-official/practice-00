4242575f388a580bfe86cdc1eab3fb961c9773c4
zahid-official <zahid.official8@gmail.com>
2024-12-13 15:15:07 +0600

    Undefine

---
+++ programming-hero/module-32/undefined.js
@@ -0,0 +1,49 @@
+// no: 01
+let a;
+// console.log(a);
+
+// no: 02
+function temp(a, b){
+    const total = a+b;
+}
+// console.log(temp(5, 7));
+
+
+// no: 03
+function number(a, b, c){
+    // return console.log(a,b,c);
+}
+number(5, 8);
+
+
+// no: 04
+function age(a, b){
+    if(a < 5 || b < 5){
+        return
+    }
+    return a + b;
+}
+// console.log(age(4, 3));
+
+
+
+// no: 05
+const man = {name: 'abir', age: 25}
+// console.log(man.age, man.salary);
+
+
+// no: 06
+const num = [1,2,3];
+// console.log(num[2], num[3]);
+
+
+
+// no: 07
+const bus = [5,6,7];
+delete bus[0];
+// console.log(bus[0], bus[1]);
+
+
+// no: 08
+const z = undefined;
+// console.log(z);
