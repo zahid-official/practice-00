92c0ba7beacbaf81fc896f04cf8da433090cde38
zahid-official <zahid.official8@gmail.com>
2024-12-13 15:14:41 +0600

    Equal

---
+++ programming-hero/module-32/equal.js
@@ -0,0 +1,79 @@
+// no: 1
+// const a = 2;
+// const b = '2';
+
+// if(a == b){
+//     console.log('1. both are equals');
+// }
+// else{
+//     console.log('1. both are different');
+// }
+
+// if(a === b){
+//     console.log('2. both are equals');
+// }
+// else{
+//     console.log('2. both are different');
+// }
+
+
+
+// // no: 2
+// const a = 0;
+// const b = false;
+
+// if(a == b){
+//     console.log('1. both are equals');
+// }
+// else{
+//     console.log('1. both are different');
+// }
+
+// if(a === b){
+//     console.log('2. both are equals');
+// }
+// else{
+//     console.log('2. both are different');
+// }
+
+
+
+
+
+// // no: 3
+// const a = [];
+// const b = [];
+
+// if(a == b){
+//     console.log('1. both are equals');
+// }
+// else{
+//     console.log('1. both are different');
+// }
+
+// if(a === b){
+//     console.log('2. both are equals');
+// }
+// else{
+//     console.log('2. both are different');
+// }
+
+
+
+// no: 3
+const a = [];
+const b = a;
+
+if(a == b){
+    console.log('1. both are equals');
+}
+else{
+    console.log('1. both are different');
+}
+
+if(a === b){
+    console.log('2. both are equals');
+}
+else{
+    console.log('2. both are different');
+}
