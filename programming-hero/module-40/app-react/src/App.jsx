e944ee8de5ddc2cad7641fdfd56d7ba714868079
zahid-official <zahid.official8@gmail.com>
2024-12-27 19:25:36 +0600

    app

---
+++ programming-hero/module-40/app-react/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Countries from './Countries/Countries'
+
+function App() {
+
+  return (
+    <>
+      <h1>React Wold Tour</h1>
+
+      <Countries></Countries>
+    </>
+  )
+}
+
+export default App
