6148838a84721d7de1cb04a75485e187aed75cab
zahid-official <zahid.official8@gmail.com>
2024-12-30 15:46:54 +0600

    Calculator

---
+++ programming-hero/module-41/component/Calculator.jsx
@@ -0,0 +1,17 @@
+const add = (first, second)=>{
+    return first + second;
+}
+
+const subtract = (first, second)=>{
+    return first - second;
+}
+
+const multiply = (first, second)=>{
+    return first * second;
+}
+
+const testAliasToDivition = (first, second)=>{
+    return first / second;
+}
+
+export {add, subtract, multiply, testAliasToDivition}
