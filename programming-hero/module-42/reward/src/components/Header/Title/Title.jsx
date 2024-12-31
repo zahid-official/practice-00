6bd84ad0c6b7cf8ecfaae8244954bcc861b3c113
zahid-official <zahid.official8@gmail.com>
2024-12-31 22:45:05 +0600

    title

---
+++ programming-hero/module-42/reward/src/components/Header/Title/Title.jsx
@@ -0,0 +1,12 @@
+
+
+const Title = () => {
+    return (
+        <div className="text-center my-16 space-y-4 mx-2">
+            <h2 className="sm:text-5xl text-3xl font-bold">Our Recipes</h2>
+            <p className="text-[#150B2B99]">Lorem ipsum dolor sit amet consectetur. Proin et feugiat senectus vulputate netus pharetra rhoncus. <br className="lg:block hidden" /> Eget urna volutpat curabitur elementum mauris aenean neque.</p>
+        </div>
+    );
+};
+
+export default Title;
