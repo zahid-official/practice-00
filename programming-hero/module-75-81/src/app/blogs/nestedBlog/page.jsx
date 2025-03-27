f52cbfd0862383b1dba83d7ad336f281c5ca002e
zahid-official <zahid.official8@gmail.com>
2025-03-28 03:45:15 +0600

    Nested Routing

---
+++ programming-hero/module-75-81/src/app/blogs/nestedBlog/page.jsx
@@ -0,0 +1,11 @@
+
+
+const NestedBlog = () => {
+    return (
+        <div>
+            <h2 className="text-3xl">This is Nested Blog Page</h2>
+        </div>
+    );
+};
+
+export default NestedBlog;
--- programming-hero/module-75-81/src/app/blogs/page.jsx
+++ programming-hero/module-75-81/src/app/blogs/page.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import Link from "next/link";
+import React from "react";
 
 const Blogs = () => {
-    return (
-        <div>
-            <h2 className='text-3xl'>This is Blogs Page</h2>
-        </div>
-    );
+  return (
+    <div>
+      <h2 className="text-3xl">This is Blogs Page</h2>
+      <p className="text-center mt-4 underline text-blue-400">
+        <Link href={"/blogs/nestedBlog"}>View Nested Blog Page</Link>
+      </p>
+    </div>
+  );
 };
 
 export default Blogs;
