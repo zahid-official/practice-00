c3c39a884be57c85f575446ff7971f5acf397385
zahid-official <zahid.official8@gmail.com>
2025-03-28 04:59:52 +0600

    Private Router

--- programming-hero/module-75-81/src/app/blogs/page.jsx
+++ programming-hero/module-75-81/src/app/blogs/page.jsx
@@ -1,12 +1,31 @@
+"use client"
+
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const Blogs = () => {
+    // useHooks
+    const router = useRouter();
+
+    const isUserLogged = true;
+    // conditional rendering for private route
+    const handlePrivate = ()=> {
+        if(isUserLogged){
+           router.push("/blogs/privateRouter");
+        }
+        else{
+            router.push("/");
+        }
+    }
+
   return (
     <div>
       <h2 className="text-3xl">This is Blogs Page</h2>
       <p className="text-center mt-4 underline text-blue-400">
-        <Link href={"/blogs/nestedBlog"}>View Nested Blog Page</Link>
+        <Link href={"/blogs/nestedBlog"}>View Nested Blog Page</Link><br /><br />
+
+        <button type="button" className="btn btn-neutral" onClick={handlePrivate}>Private Route</button>
       </p>
     </div>
   );
---
+++ programming-hero/module-75-81/src/app/blogs/privateRouter/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PrivateRouter = () => {
+    return (
+        <div>
+            <h2 className="text-3xl italic font-semibold">Only those who are logged in have access to this private router</h2>
+        </div>
+    );
+};
+
+export default PrivateRouter;
